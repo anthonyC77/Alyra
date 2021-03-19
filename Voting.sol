71cb9e177b2310e2909e94f58d03bcb30f05ba54
anthonyC77 <35571461+anthonyC77@users.noreply.github.com>
2021-03-20 00:43:08 +0100

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -37,7 +37,50 @@ contract Voting {
         VotesTallied
     }
     
+    WorkflowStatus status;
+    
+    Proposal[] Proposals;
+    
+    function voterRegister(address _address)  public {
+        
+        
+        emit VoterRegistered(_address);
+    }
+    
+    function StartProposalsRegistration() public{
+        // if admin 
+        //Proposals = new Proposal[];
+        
+        emit ProposalsRegistrationStarted();
+    }
+    
+    function StopProposalsRegistration() public{
+        // if admin 
+         emit ProposalsRegistrationEnded();
+    }
+    
+    function ProposeRecord(address _address, string memory _descriptionProposal) public {
+        // une propsition par user ?
+        Proposals.push(Proposal(_descriptionProposal, 0));
+        uint proposalId = Proposals.length - 1;
+        emit ProposalRegistered(proposalId);
+    }
     
     
+    function SartVote() public{
+        status = WorkflowStatus.VotingSessionStarted;
+        emit VotingSessionStarted();
+    }
+    
+    function StopVote() public{
+        status = WorkflowStatus.VotingSessionEnded;
+        emit VotingSessionEnded();
+    }
+    
+    function VoteUser(address _address, uint propoalId) public {
+        // if already vote no require
+        require(status ==  WorkflowStatus.VotingSessionStarted, "Error...");
+        emit Voted(_address, propoalId);
+    }
     
 }
