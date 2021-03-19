cc0dc55d2d0151fa79f38b1de9124ba1d322e716
anthonyC77 <35571461+anthonyC77@users.noreply.github.com>
2021-03-19 16:00:39 +0100

    Create Voting.sol

---
+++ Voting.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+pragma solidity =0.8.0;
+
+contract Voting {
+    
+    event VoterRegistered(address voterAddress);
+    event ProposalsRegistrationStarted();
+    event ProposalsRegistrationEnded();
+    event ProposalRegistered(uint proposalId);
+    event VotingSessionStarted();
+    event VotingSessionEnded();
+    event Voted (address voter, uint proposalId);
+    event VotesTallied();
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus
+    newStatus);
+    
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+    
+    
+    
+    
+}
